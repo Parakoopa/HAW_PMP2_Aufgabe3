9eb80525f0fba958bd114c61c118c8eb27aa8384
T.Beier <tim_beier@hotmail.de>
2016-12-18 20:48:30 +0100

    more stuff that marco said

--- src/main/java/kalender/tim/TerminkalenderController.java
+++ src/main/java/kalender/tim/TerminkalenderController.java
@@ -3,11 +3,15 @@ package kalender.tim;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import kalender.interfaces.Datum;
 import kalender.interfaces.Termin;
 import kalender.interfaces.TerminKalender;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 public class TerminkalenderController {
 
@@ -15,36 +19,69 @@ public class TerminkalenderController {
     private Tab monatsansicht;
 
     @FXML
-    private TableColumn<?, ?> monatsansicht_Tab_Datum;
+    private TableView<TerminImpl> monatsansicht_Tableview;
+
+    @FXML
+    private TableView<?> tagesAnsicht_Tableview;
+
+    @FXML
+    private TableColumn<Termin, String> monatsansicht_Tab_Datum;
 
     @FXML
     private TitledPane tagesAnsicht_Tab;
 
     @FXML
-    private TableColumn<?, ?> tagesAnsicht_Tab_Datum;
+    private TableColumn<Termin, String> tagesAnsicht_Tab_Datum;
 
     @FXML
-    private TableColumn<?, ?> tagesAnsicht_Tab_Beschreibung;
+    private TableColumn<Termin, String> tagesAnsicht_Tab_Beschreibung;
 
     @FXML
     private Tab tagesAnsicht;
 
     @FXML
-    private TableColumn<?, ?> monatsansicht_Tab_Datum1;
+    private TableColumn<Termin, String> monatsansicht_Tab_Datum1;
+
+    @FXML
+    private DatePicker datePicker;
+
 
     TerminlistenGenerator terminlistenGenerator = new TerminlistenGenerator();
     TerminKalender terminKalender = terminlistenGenerator.erstelleTermine();
 
     protected void initialize() {
-        MonatImpl monat = new MonatImpl(1992,2);
-        Termin[] termine = terminKalender.termineFuerMonat(monat, new DatumImpl());
-        fuegeTermineInTabelle(terminKalender.termineFuerMonat(new MonatImpl(1992,1)), monatsansicht_Tab_Datum);
+        TableColumn<TerminImpl,String> nameColumn = new TableColumn<>("Name");
+        monatsansicht_Tableview.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("datum"));
+
+        monatsansicht_Tableview.setItems(getTermine(terminKalender));
+
+        //datePicker.setOnAction(this::onDatePickerAction); //ToDo Remove error
+        datePicker.setValue(LocalDate.now());
+        datePicker.getValue().getYear();
+
+       // MonatImpl monat = new MonatImpl(1992,2);
+       // Termin[] termine = terminKalender.termineFuerMonat(monat, new DatumImpl());
+
     }
+
+    private ObservableList<TerminImpl> getTermine(TerminKalender terminKalender) {
+        ObservableList<TerminImpl> obsTermine = FXCollections.observableArrayList();
+        Map<Datum, List<Termin>> termine = terminKalender.termineFuerTag(new TagImpl(1, 1));
+
+        for(List<Termin> termin : termine.values()) {
+            obsTermine.add((TerminImpl) termin);
+        }
+        return obsTermine;
+    }
+
     private void fuegeTermineInTabelle(Termin[] termine, TableColumn tableColumn) {
         for(Termin termin : termine) {
         //    tableColumn.setCellValueFactory("stuff");
         }
+
     }
+
+
 
 
 }
